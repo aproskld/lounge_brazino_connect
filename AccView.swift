caeb327ae0765aa1acdf584084e625f293300a72
aproskld <95758406+aproskld@users.noreply.github.com>
2024-03-05 19:15:28 +0200

    Update AccView.swift

--- AccView.swift
+++ AccView.swift
@@ -5,58 +5,44 @@
     "categories": [
       {
         "id": 1,
-        "name": "Gifts Coctails",
-        "img": "https://i.ibb.co/JHCQBQs/mainorange.jpg",
+        "name": "Bonus Menu",
+        "img": "https://i.ibb.co/JHCQBQs.jpg",
         "price": 30,
         "created_at": null,
         "goods": [
           {
-            "id": 101,
-            "name": "Refreshing strawberries",
-            "img": "https://i.ibb.co/cyTZkyJ/straw.jpg",
+            "id": 10,
+            "name": "Brazino Beer",
+            "img": "https://i.ibb.co/1sRX2Ry/brazinobeer.jpg",
             "price": 300,
+            "mass": "500g"
+          },
+          {
+            "id": 12,
+            "name": "DarkBraz Beer",
+            "img": "https://i.ibb.co/DQPRWSF/darckbeer.jpg",
+            "price": 350,
+            "mass": "500g"
+          },
+          {
+            "id": 13,
+            "name": "RedBraz Beer",
+            "img": "https://i.ibb.co/RgQ8GnF/Redbeer.jpg",
+            "price": 300,
+            "mass": "500g"
+          },
+          {
+            "id": 14,
+            "name": "Spicy",
+            "img": "https://i.ibb.co/tPbpgXG/spicy.jpg",
+            "price": 400,
             "mass": "200g"
           },
           {
-            "id": 102,
-            "name": "Strawberry sangria",
-            "img": "https://i.ibb.co/rxfdHYB/sangria.jpg",
-            "price": 300,
-            "mass": "250g"
-          },
-          {
-            "id": 103,
-            "name": "Smash",
-            "img": "https://i.ibb.co/MZ0Yjxd/smash.jpg",
-            "price": 500,
-            "mass": "250g"
-          },
-          {
-            "id": 104,
-            "name": "Negroni",
-            "img": "https://i.ibb.co/sF74vP0/negroni.jpg",
-            "price": 500,
-            "mass": "250g"
-          },
-          {
-            "id": 105,
-            "name": "Neon Club",
-            "img": "https://i.ibb.co/5861c4G/neonclubb.jpg",
-            "price": 800,
-            "mass": "250g"
-          },
-          {
-            "id": 106,
-            "name": "Neon brazil",
-            "img": "https://i.ibb.co/phxxGNM/nronbrazil.jpg",
-            "price": 900,
-            "mass": "250g"
-          },
-          {
-            "id": 107,
-            "name": "Almost James Bond",
-            "img": "https://i.ibb.co/Rbn7wV6/jamesbo.jpg",
-            "price": 900,
+            "id": 15,
+            "name": "OldBro",
+            "img": "https://i.ibb.co/kJKxf90/oldbro.jpg",
+            "price": 400,
             "mass": "250g"
           }
         ]
